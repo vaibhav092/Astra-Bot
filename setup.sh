ad2b937091b32e4ea6082d25ecfdc12211108359
Vaibhav <62978714+vaibhav092@users.noreply.github.com>
2022-06-14 17:09:23 +0530

    amass error fix

--- setup.sh
+++ setup.sh
@@ -13,10 +13,26 @@ then
 	echo "It may take some time to complete"
 	sudo apt-get update
 	sudo apt-get install -y nmap
-	sudo apt-get install -y golang-go	
+	
+	echo "Installing Golang into your Device"
+	wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+	sudo tar -xvf go1.13.5.linux-amd64.tar.gz
+	sudo mv go /usr/local
+	export GOROOT=/usr/local/go
+	export GOPATH=$HOME/go
+	export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+	echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+	echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+	echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+	source ~/.bash_profile
+	sleep 1
+        echo "Installation done " 
+        echo 
+	export GO111MODULE=on
+	go get -v github.com/OWASP/Amass/v3/...
+	echo
+	echo "Amass installation completed"	
 	sudo apt-get install python3 python3-pip
-	go install -v github.com/OWASP/Amass/v3/...@master
-	sudo cp ~/go/bin/amass /usr/bin
 	go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 	sudo cp ~/go/bin/subfinder /usr/bin
 	pip install -r requirements.txt
@@ -32,10 +48,25 @@ then
 	echo "It may take some time to complete"
 	sudo apt update
 	sudo apt install -y nmap
-	sudo apt install -y golang-go	
+	echo "Installing Golang into your Device"
+	wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+	sudo tar -xvf go1.13.5.linux-amd64.tar.gz
+	sudo mv go /usr/local
+	export GOROOT=/usr/local/go
+	export GOPATH=$HOME/go
+	export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+	echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+	echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+	echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+	source ~/.bash_profile
+	sleep 1    
+        echo "Installation done " 
+        echo 
+	export GO111MODULE=on
+	go get -v github.com/OWASP/Amass/v3/...
+	echo
+	echo "Amass installation completed"	
 	sudo apt install python3 python3-pip
-	go install -v github.com/OWASP/Amass/v3/...@master
-	sudo cp ~/go/bin/amass /usr/bin
 	go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 	sudo cp ~/go/bin/subfinder /usr/bin
 	pip install -r requirements.txt
